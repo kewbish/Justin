937668863027710e97e0b68adc930c7293847f4d
Kewbish <kewbish@gmail.com>
2020-03-16 19:45:01 -0700

    Small edits

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Justin - A personalized assistant dashboard
-A minimalist, modern interface for my personal workflows and interactions.
-Made with Python and Tkinter, March 2020 - current.
-Released under the [GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).
+A minimalist, modern interface for my personal workflows and interactions.  
+Made with Python and Tkinter, March 2020 - current.  
+Released under the [GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).  
--- justin.py
+++ justin.py
@@ -1,12 +1,6 @@
 import tkinter as t
 from tkinter import font
 import tkinter.ttk as ttk
-from webbrowser import open_new
-
-
-def open_url(url):
-    open_new(url)
-
 
 root = t.Tk()
 root.geometry("800x550")
@@ -15,8 +9,6 @@ root.configure(background="#1e1e1e")
 
 montserrat_bold = font.Font(family="Montserrat Bold", size=50)
 montserrat_light = font.Font(family="Montserrat Light", size=20)
-underline = font.Font(family="Montserrat Light", size=20)
-underline.configure(underline=True)
 
 dash = t.Frame(root)
 dash.configure(background="#1e1e1e")
@@ -29,14 +21,13 @@ header_title.configure(font=montserrat_bold, background="#1e1e1e", fg="white")
 header_separate = ttk.Separator(root, orient="vertical")
 header_separate.grid(column=2, row=1, sticky="ns", padx=15)
 
-header_quicklinks = t.Frame(root)
-header_quicklinks.grid(column=3, row=1)
+header_search_front = t.Entry(root)
+header_search_front.grid(column=3, row=1)
+header_search_front.configure(width=25, fg="white", background="#1e1e1e",
+                              font=montserrat_light, borderwidth=3)
 
-header_quicklinks_gmail = t.Label(header_quicklinks, text="Gmail")
-header_quicklinks_gmail.configure(font=underline, background="#1e1e1e",
-                                  fg="white")
-header_quicklinks_gmail.pack()
-header_quicklinks_gmail.bind("<Button-1>", lambda e: open_url(
-    "https://mail.google.com"))
+main_frame = t.Frame(root)
+main_frame.configure(background="#1e1e1e")
+main_frame.grid(column=1, row=2)
 
 root.mainloop()
