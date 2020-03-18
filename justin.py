e98eb55babda5c2db4fa1f2a3a62c5874c9dc164
Kewbish <kewbish@gmail.com>
2020-03-17 20:00:36 -0700

    Moving to CL

--- justin.py
+++ justin.py
@@ -1,9 +1,7 @@
 from os import startfile
 from subprocess import call
-import tkinter as t
-from tkinter import font
-import tkinter.ttk as ttk
 from webbrowser import open_new
+from terminaltables import SingleTable
 
 
 def socials(self):
@@ -15,38 +13,15 @@ def socials(self):
     startfile(r"C:\Users\offic\Downloads\Dev\Tools\Shortcuts\Telegram.lnk")
 
 
-root = t.Tk()
-root.geometry("800x550")
-root.title("Justin - Here to help.")
-root.configure(background="#1e1e1e")
+def vancouver(self):
+    call("bash -c 'curl wttr.in'")
 
-mb = font.Font(family="Montserrat Bold", size=50)
-mbs = font.Font(family="Montserrat Bold", size=40)
-ml = font.Font(family="Montserrat Light", size=20)
 
-dash = t.Frame(root)
-dash.configure(background="#1e1e1e")
-dash.grid(padx=15, pady=10)
-
-header_title = t.Label(root, text="Justin.")
-header_title.grid(row=1, column=1)
-header_title.configure(font=mb, background="#1e1e1e", fg="white")
-
-header_separate = ttk.Separator(root, orient="vertical")
-header_separate.grid(column=2, row=1, sticky="ns", padx=15)
-
-header_search_front = t.Entry(root)
-header_search_front.grid(column=3, row=1)
-header_search_front.configure(width=25, fg="white", background="#1e1e1e",
-                              font=ml, borderwidth=3)
-
-main_frame = t.Frame(root)
-main_frame.configure(background="#1e1e1e")
-main_frame.grid(columnspan=3, column=1, row=2, pady=10)
-
-main_new_day = t.Button(main_frame, text="Socials.")
-main_new_day.configure(background="#252525", fg="white", font=mbs)
-main_new_day.grid(column=1, row=1)
-main_new_day.bind('<Button-1>', socials)
-
-root.mainloop()
+print("""       _           _   _         
+      | |         | | (_)        
+      | |_   _ ___| |_ _ _ __    
+  _   | | | | / __| __| | '_ \   
+ | |__| | |_| \__ \ |_| | | | |_ 
+  \____/ \__,_|___/\__|_|_| |_(_)""")
+options = [["Program", "What it does"], ["Socials", "Opens fresh social media."]]
+print(SingleTable(options, title="Here to help.").table)
