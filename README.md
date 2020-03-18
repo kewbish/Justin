6f58988288f2194b8a6a2e6e3fe0a6407f9a1cee
Kewbish <kewbish@gmail.com>
2020-03-18 13:49:27 -0700

    Small edits

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Justin - A personalized assistant dashboard
-A minimalist, modern interface for my personal workflows and interactions.  
+A minimalist, modern CLI for my personal workflows and interactions.  
 Made with Python and Tkinter, March 2020 - current.  
 Released under the [GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html).  
+## Setup
+Run `justin.cmd` in Terminal after downloading the files and moving `justin.py` into the `C:\Users\offic\Downloads\Dev\Justin\` folder. Alternatively, edit the `.cmd` file.  
+The Linux / Mac version of the DOSKEY command is `alias`. Run `alias justin='python \path\to\file $*'` to setup an alias for Justin.  
+## Workflows and Features
+Workflows:
+- `justin socials` - opens social media
+Features:
+- Infrastructure to support infinite workflows and commands
+- Pretty ASCII!
--- justin.py
+++ justin.py
@@ -1,10 +1,11 @@
 from os import startfile
 from subprocess import call
+from sys import argv
 from webbrowser import open_new
 from terminaltables import SingleTable
 
 
-def socials(self):
+def socials():
     open_new("https://mail.google.com/mail/u/0")
     open_new("https://discordapp.com/channels/@me")
     open_new("https://app.slack.com/client/TFFEQ2X61/CTUSAU05S")
@@ -13,15 +14,18 @@ def socials(self):
     startfile(r"C:\Users\offic\Downloads\Dev\Tools\Shortcuts\Telegram.lnk")
 
 
-def vancouver(self):
-    call("bash -c 'curl wttr.in'")
+def help():
+    print(r"""       _           _   _
+        | |         | | (_)
+        | |_   _ ___| |_ _ _ __
+    _   | | | | / __| __| | '_ \
+    | |__| | |_| \__ \ |_| | | | |_
+    \____/ \__,_|___/\__|_|_| |_(_)\n""")
+    options = [["Program", "What it does"], [
+        "Socials", "Opens fresh social media."]]
+    print(SingleTable(options, title="Here to help.").table)
 
 
-print("""       _           _   _         
-      | |         | | (_)        
-      | |_   _ ___| |_ _ _ __    
-  _   | | | | / __| __| | '_ \   
- | |__| | |_| \__ \ |_| | | | |_ 
-  \____/ \__,_|___/\__|_|_| |_(_)""")
-options = [["Program", "What it does"], ["Socials", "Opens fresh social media."]]
-print(SingleTable(options, title="Here to help.").table)
+a = argv[-1]
+print(a)
+input("Press any key to exit...")
