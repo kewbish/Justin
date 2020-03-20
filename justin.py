ce6dbcfb173e9c4d1c2c1d5ad87373b4e3557b44
Kewbish <kewbish@gmail.com>
2020-03-20 16:56:58 -0700

    Implemented GH actions

--- justin.py
+++ justin.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from logging import debug
-from os import getenv, startfile, system
+from os import getenv, popen, startfile, system
 from requests import get
 from sys import argv
 from webbrowser import open_new
@@ -20,8 +20,7 @@ def socials():
 def local():
     system("bash -c 'curl wttr.in?0'")
     cw = get("https://coronavirus-tracker-api.herokuapp.com/v2/latest").json()
-    bc = get("https://coronavirus-tracker-api.herokuapp.com/v2/",
-             "locations?country_code=CA").json()
+    bc = get("https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=CA").json()
     cor_table = [["Latest", "World", "BC"],
                  ["Cases", cw['latest']['confirmed'],
                   bc['locations'][0]['latest']['confirmed']],
@@ -29,9 +28,10 @@ def local():
                   bc['locations'][0]['latest']['deaths']],
                  ["Recoveries", cw['latest']['recovered'],
                   bc['locations'][0]['latest']['recovered']]]
+    print("\n")
     print(SingleTable(cor_table, title="Coronavirus Updates").table)
     system("bash -c 'cal'")
-    print("Current time is:")
+    print("\nCurrent time is:")
     system("time /t")
     debug("Opened local information.")
 
@@ -43,6 +43,22 @@ def news():
     for a in news["articles"]:
         print(f"⚬ {a['title']} - {a['description']}")
     print("\nNews courtesy of the NewsAPI - https://newsapi.org")
+    debug("Printed news to terminal.")
+
+
+def github_issues():
+    print("Currently, these are the issues you have open.")
+    p = system("bash -c 'curl -u 'kewbish' https://api.github.com/issues'")
+    debug("Opened issues.")
+
+
+def github_init():
+    gh_repo_name = argv[2]
+    system("git init")
+    system(f"git remote add https://github.com/kewbish/{gh_repo_name}.git")
+    print(f"Setting up at: https://github.com/kewbish/{gh_repo_name}.git")
+    system("git add . && github")
+    debug("Set up repo.")
 
 
 def help():
@@ -55,7 +71,10 @@ def help():
  """)
     options = [["Program", "What it does"],
                ["socials", "Opens fresh social media tabs."],
-               ["local", "Bringing local information to terminal."]]
+               ["local", "Bringing local information to terminal."],
+               ["news", "Prints national news thru NewsAPI."],
+               ["ghissues", "Notes open issues - req. auth."],
+               ["ghinit", "Prepares Git repo for use."]]
     print(SingleTable(options, title="Here to help.").table)
     print("Usage: justin [program] [options]")
 
@@ -72,6 +91,10 @@ try:
         local()
     elif a == "news":
         news()
+    elif a == "ghissues":
+        github_issues()
+    elif a == "ghinit":
+        github_init()
     elif a == "help":
         help()
     else:
