a298f21affdcbfdfaf3a217d9db37007d7bed6b6
Kewbish <kewbish@gmail.com>
2020-03-20 13:54:02 -0700

    Added news

--- justin.py
+++ justin.py
@@ -1,7 +1,7 @@
+from dotenv import load_dotenv
 from logging import debug
-from os import startfile
+from os import getenv, startfile, system
 from requests import get
-from subprocess import call
 from sys import argv
 from webbrowser import open_new
 from terminaltables import SingleTable
@@ -18,30 +18,41 @@ def socials():
 
 
 def local():
-    call("bash -c 'curl wttr.in'")
+    system("bash -c 'curl wttr.in?0'")
     cw = get("https://coronavirus-tracker-api.herokuapp.com/v2/latest").json()
-    bc = get("https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=CA").json()
+    bc = get("https://coronavirus-tracker-api.herokuapp.com/v2/",
+             "locations?country_code=CA").json()
     cor_table = [["Latest", "World", "BC"],
                  ["Cases", cw['latest']['confirmed'],
-                 bc['locations'][0]['latest']['confirmed']],
+                  bc['locations'][0]['latest']['confirmed']],
                  ["Deaths", cw['latest']['deaths'],
-                 bc['locations'][0]['latest']['deaths']],
+                  bc['locations'][0]['latest']['deaths']],
                  ["Recoveries", cw['latest']['recovered'],
-                 bc['locations'][0]['latest']['recovered']]]
+                  bc['locations'][0]['latest']['recovered']]]
     print(SingleTable(cor_table, title="Coronavirus Updates").table)
-    call("bash -c 'cal'")
+    system("bash -c 'cal'")
     print("Current time is:")
-    call("time /t")
+    system("time /t")
     debug("Opened local information.")
+
+
+def news():
+    load_dotenv(r"C:\Users\offic\Downloads\Dev\Justin\files\.env")
+    key = getenv("NEWSAPI")
+    news = get(f"http://newsapi.org/v2/top-headlines?country=ca&apiKey={key}").json()
+    for a in news["articles"]:
+        print(f"⚬ {a['title']} - {a['description']}")
+    print("\nNews courtesy of the NewsAPI - https://newsapi.org")
 
 
 def help():
     print(r"""       _           _   _
-        | |         | | (_)
-        | |_   _ ___| |_ _ _ __
-    _   | | | | / __| __| | '_ \
-    | |__| | |_| \__ \ |_| | | | |_
-    \____/ \__,_|___/\__|_|_| |_(_)\n""")
+      | |         | | (_)
+      | |_   _ ___| |_ _ _ __
+  _   | | | | / __| __| | '_ \
+ | |__| | |_| \__ \ |_| | | | |_
+  \____/ \__,_|___/\__|_|_| |_(_)
+ """)
     options = [["Program", "What it does"],
                ["socials", "Opens fresh social media tabs."],
                ["local", "Bringing local information to terminal."]]
@@ -59,6 +70,8 @@ try:
         socials()
     elif a == "local":
         local()
+    elif a == "news":
+        news()
     elif a == "help":
         help()
     else:
